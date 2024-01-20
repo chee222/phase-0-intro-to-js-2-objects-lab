4946a5c07ba627d1c043adb45b64e6cf1849e763
Chee <ck_white@outlook.com>
2024-01-20 13:13:24 -0500

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
+
+const employee = {
+  name: 'Sam',
+  streetAddress: "11 Broadway",
+  
+};
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "streetAddress", "11 Broadway");
+
+console.log("Original Employee:", employee);
+console.log("Updated Employee:", updatedEmployeeup);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+expect(employee, 'streetAddress', '12 Broadway').to.eql({
+  name: 'Sam',
+  streetAddress: '12 Broadway',
+});
+
+expect (employee).to.eql({
+  name: 'Sam',
+  streetAddress: '12 Broadway',
+});
+
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  let newEmployee = { ...employee };
+   delete newEmployee[key];
+  return newEmployee;
+}
+
+let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'undefined');
+
+employee.name = 'Sam';
+
+console.log(employee);
+
+
+
+
